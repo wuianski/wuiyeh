1c32be54dff02c8718a62690d7c9aefca616710b
wuianski <wuiyeh@gmail.com>
2025-03-03 21:51:08 +0800

    fixed build error

--- app/layout.jsx
+++ app/layout.jsx
@@ -137,127 +137,6 @@ async function getCV() {
   }
 }
 
-const cat1 = await getCat1();
-const cat2 = await getCat2();
-const cat3 = await getCat3();
-const cat4 = await getCat4();
-const cat5 = await getCat5();
-const cv = await getCV();
-// console.log(cv);
-
-/* Orgnize navigation from works */
-const NAVIGATION = [
-  {
-    kind: "header",
-    title: cat2[0].category.category_name.toUpperCase(),
-  },
-  ...cat2.map((work) => ({
-    segment: `${work.slug}`,
-    title: work.title,
-    icon: (
-      <Box
-        component="img"
-        sx={{ height: 30 }}
-        alt="project cover image"
-        src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
-      />
-    ),
-  })),
-
-  {
-    kind: "header",
-    title: cat1[0].category.category_name.toUpperCase(),
-  },
-  ...cat1.map((work) => ({
-    segment: `${work.slug}`,
-    title: work.title,
-    icon: (
-      <Box
-        component="img"
-        sx={{ height: 30 }}
-        alt="project cover image"
-        src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
-      />
-    ),
-  })),
-
-  {
-    kind: "header",
-    title: cat5[0].category.category_name.toUpperCase(),
-  },
-  ...cat5.map((work) => ({
-    segment: `${work.slug}`,
-    title: work.title,
-    icon: (
-      <Box
-        component="img"
-        sx={{ height: 30 }}
-        alt="project cover image"
-        src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
-      />
-    ),
-  })),
-
-  {
-    kind: "header",
-    title: cat3[0].category.category_name.toUpperCase(),
-  },
-  ...cat3.map((work) => ({
-    segment: `${work.slug}`,
-    title: work.title,
-    icon: (
-      <Box
-        component="img"
-        sx={{ height: 30 }}
-        alt="project cover image"
-        src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
-      />
-    ),
-  })),
-
-  {
-    kind: "header",
-    title: cat4[0].category.category_name.toUpperCase(),
-  },
-  ...cat4.map((work) => ({
-    segment: `${work.slug}`,
-    title: work.title,
-    icon: (
-      <Box
-        component="img"
-        sx={{ height: 30 }}
-        alt="project cover image"
-        src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
-      />
-    ),
-  })),
-
-  {
-    kind: "header",
-    title: "",
-  },
-  {
-    kind: "divider",
-  },
-  {
-    kind: "header",
-    title: (
-      <>
-        <Box component="span">CV: </Box>
-        <Box component="span">
-          <a
-            href={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${cv.cv.filename_disk}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Download
-          </a>
-        </Box>
-      </>
-    ),
-  },
-];
-
 /* SEO */
 export const metadata = {
   title: "WU I-YEH | 吳宜曄",
@@ -267,6 +146,127 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   // const works = await getWorks();
   // console.log(works);
+  const cat1 = await getCat1();
+  const cat2 = await getCat2();
+  const cat3 = await getCat3();
+  const cat4 = await getCat4();
+  const cat5 = await getCat5();
+  const cv = await getCV();
+  // console.log(cv);
+
+  /* Orgnize navigation from works */
+  const NAVIGATION = [
+    {
+      kind: "header",
+      title: cat2[0].category.category_name.toUpperCase(),
+    },
+    ...cat2.map((work) => ({
+      segment: `${work.slug}`,
+      title: work.title,
+      icon: (
+        <Box
+          component="img"
+          sx={{ height: 30 }}
+          alt="project cover image"
+          src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
+        />
+      ),
+    })),
+
+    {
+      kind: "header",
+      title: cat1[0].category.category_name.toUpperCase(),
+    },
+    ...cat1.map((work) => ({
+      segment: `${work.slug}`,
+      title: work.title,
+      icon: (
+        <Box
+          component="img"
+          sx={{ height: 30 }}
+          alt="project cover image"
+          src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
+        />
+      ),
+    })),
+
+    {
+      kind: "header",
+      title: cat5[0].category.category_name.toUpperCase(),
+    },
+    ...cat5.map((work) => ({
+      segment: `${work.slug}`,
+      title: work.title,
+      icon: (
+        <Box
+          component="img"
+          sx={{ height: 30 }}
+          alt="project cover image"
+          src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
+        />
+      ),
+    })),
+
+    {
+      kind: "header",
+      title: cat3[0].category.category_name.toUpperCase(),
+    },
+    ...cat3.map((work) => ({
+      segment: `${work.slug}`,
+      title: work.title,
+      icon: (
+        <Box
+          component="img"
+          sx={{ height: 30 }}
+          alt="project cover image"
+          src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
+        />
+      ),
+    })),
+
+    {
+      kind: "header",
+      title: cat4[0].category.category_name.toUpperCase(),
+    },
+    ...cat4.map((work) => ({
+      segment: `${work.slug}`,
+      title: work.title,
+      icon: (
+        <Box
+          component="img"
+          sx={{ height: 30 }}
+          alt="project cover image"
+          src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${work.cover.filename_disk}`}
+        />
+      ),
+    })),
+
+    {
+      kind: "header",
+      title: "",
+    },
+    {
+      kind: "divider",
+    },
+    {
+      kind: "header",
+      title: (
+        <>
+          <Box component="span">CV: </Box>
+          <Box component="span">
+            <a
+              href={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${cv.cv.filename_disk}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Download
+            </a>
+          </Box>
+        </>
+      ),
+    },
+  ];
+
   return (
     /* Must to have data-toolpad-color-scheme="dark", otherwise there is a server error. */
     <html
@@ -282,7 +282,7 @@ export default async function RootLayout({ children }) {
               <DashboardLayout
                 // defaultSidebarCollapsed
                 disableCollapsibleSidebar
-                sidebarExpandedWidth={320}
+                sidebarExpandedWidth={350}
                 slots={{
                   appTitle: CustomAppTitle,
                   sidebarFooter: SidebarFooter,
